23ad004e397e5d9df344781f6129085782d8f296
Vstark0 <105135745+Vstark0@users.noreply.github.com>
2022-08-31 21:15:56 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/api/users", (req, res) => {
+    let friends = ["ciaran", "nicole", "mara", "logan"];
+    res.status(200).send(friend);
+});
+app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+    res.status(200).send(phrase);
+
+});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
+
+app.get("/api/users")
+
+
+
+
+
+
+
+
